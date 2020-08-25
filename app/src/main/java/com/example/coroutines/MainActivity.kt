8d3d2dd98a6ea8b27b2ba0b8155823387b0c92f6
Zp <tripxlife@gmail.com>
2020-08-25 19:15:32 -0400

    0.1. with changes with Daniel Corvalan L. 2.0

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.core.net.toUri
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
@@ -22,37 +23,39 @@ import java.net.URL
 class MainActivity : AppCompatActivity() {
     //private lateinit var mProgressBAR : ProgressBar           CON KOTLIN SYNTETIC NO ES NECESARIO
     //private lateinit var mSpaceImage : ImageView
-
-
+    private lateinit var progressBARList: List<ProgressBar>
+    private lateinit var imagesofspace: List<ImageView>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this, "Running in Main Thread", Toast.LENGTH_SHORT).show()
+        })
         //val url = "https://apod.nasa.gov/apod/image/1908/M61-HST-ESO-S1024.jpg" - 1 -
         val urls = listOf<String>("https://apod.nasa.gov/apod/image/1908/M61-HST-ESO-S1024.jpg",
             "https://apod.nasa.gov/apod/image/1908/M61-HST-ESO-S1024.jpg",
             "https://apod.nasa.gov/apod/image/1908/M61-HST-ESO-S1024.jpg",
             "https://apod.nasa.gov/apod/image/1908/M61-HST-ESO-S1024.jpg")
-        val progressBARList = listOf(progressBAR, progressBAR1, progressBAR2, progressBAR3)
-        val imagesofspace = listOf(nasa_image,nasa_image1,nasa_image2,nasa_image3)
-
+            progressBARList = listOf(progressBAR, progressBAR1, progressBAR2, progressBAR3)
+            imagesofspace = listOf(nasa_image,nasa_image1,nasa_image2,nasa_image3)
             CoroutineScope(Dispatchers.Main).launch {
-                doInBackground(urls,progressBARList,imagesofspace)
+                for (i in 0 until urls.size) {
+                progressBARList[i].visibility = View.VISIBLE
+                    var img = doInBackground1(urls[i])
+                    progressBARList[i].visibility= View.GONE
+                    imagesofspace[i].setImageBitmap(img)
+                }
             }
     }
-    suspend fun doInBackground(urls:List<String>, progressBARlist: List<ProgressBar>, imageList: List<ImageView>){
-        //var urlDisplay = url
-        var listaZIP = urls.zip(progressBARlist)
-        var j = imageList.iterator()
-        for ((url, progressB) in listaZIP) {
+    suspend fun doInBackground(urls:List<String>){
+        for (i in 0 until urls.size) {
         withContext(Dispatchers.Default) {
                 try {
-                    progressB.visibility = View.VISIBLE
-                    val inputStream = java.net.URL(url).openStream()
+                    progressBARList[i].visibility = View.VISIBLE
+                    val inputStream = java.net.URL(urls[i]).openStream()
                     var result = BitmapFactory.decodeStream(inputStream)
-                    var imagen = j.next()
-                    imagen.setImageBitmap(result)
-                    Log.e("VISIBLE", "pass")
-                    // bitMAP = BitmapFactory.decodeStream(inputStream)       ANTES
+                    imagesofspace[i].setImageBitmap(result)
+                    progressBARList[i].visibility = View.GONE
                 } catch (e: Exception) {
                     Log.e("eRRoR", e.message.toString())
                     e.printStackTrace()
@@ -60,9 +63,20 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
-    //fun updateVIEW(result: Bitmap){
-           //progressBAR.visibility = View.GONE
-        // nasa_image.setImageBitmap(result)
-   // }
+    suspend fun doInBackground1(url:String): Bitmap?{
+        var bitMAP: Bitmap?= null
+        withContext(Dispatchers.Default) {
+            try {val inputStream = java.net.URL(url).openStream()
+                bitMAP = BitmapFactory.decodeStream(inputStream)
+            } catch (e: Exception) {
+                Log.e("eRRoR", e.message.toString())
+                e.printStackTrace()
+            }
+        }
+        return bitMAP
+    }
+    fun updateVIEW(result: Bitmap){
+         progressBAR.visibility = View.GONE
+         nasa_image.setImageBitmap(result)
+    }
 }
